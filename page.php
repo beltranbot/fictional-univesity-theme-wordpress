015e3f12442d9720ed03da3878442f15154a5639
beltranbot <beltranbot@gmail.com>
2022-09-15 18:22:51 -0500

    setting up styles and design

--- page.php
+++ page.php
@@ -1,6 +1,15 @@
-<?php while(have_posts()) {
+<?php
+
+  get_header();
+
+  while(have_posts()) {
     the_post(); ?>
+    <h1>This is a page not a post</h1>
     <h2><?php the_title(); ?></h2>
-    <h2>This is a page not a post</h2>
     <?php the_content(); ?>
-<?php } ?>
+    
+  <?php }
+
+  get_footer();
+
+?>
