f9a2396c43fac89b559f636c6a8a5a350d3913f9
beltranbot <beltranbot@gmail.com>
2022-09-18 18:28:18 -0500

    single blog entry page

--- index.php
+++ index.php
@@ -13,13 +13,13 @@
     <?php while(have_posts()): ?>
       <?php the_post(); ?>
       <div class="post-item">
-        <h2 class="headline headline--medium headline--post-title><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+        <h2 class="headline headline--medium headline--post-title"><a href="<?php echo the_permalink(get_the_ID()); ?>"><?php the_title(); ?></a></h2>
         <div class="metabox">
           <p>Posted by <?php the_author_posts_link(); ?> on <?php the_time("n.j.y") ?> in <?php echo get_the_category_list(", ") ?></p>
         </div>
         <div class="generic-content">
             <?php the_excerpt(); ?>
-            <p><a class="btn btn--blue" href="phptag">Continue reading &raquo;</a></p>
+            <p><a class="btn btn--blue" href="<?php echo the_permalink(); ?>">Continue reading &raquo;</a></p>
           </div>
       </div>
     <?php endwhile; ?>
