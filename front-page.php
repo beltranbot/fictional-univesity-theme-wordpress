fd97ff95177670f9102250946b681e8623019185
beltranbot <beltranbot@gmail.com>
2022-09-18 20:55:09 -0500

    adding navigation for custom post type event

--- front-page.php
+++ front-page.php
@@ -29,12 +29,19 @@
                     </a>
                     <div class="event-summary__content">
                         <h5 class="event-summary__title headline headline--tiny"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h5>
-                        <p><?php echo wp_trim_words(get_the_content(), 18); ?> <a href="<?php the_permalink();  ?>" class="nu gray">Learn more</a></p>
+                        <p>
+                            <?php if (has_excerpt()) : ?>
+                                <?php echo get_the_excerpt(); ?>
+                            <?php else : ?>
+                                <?php echo wp_trim_words(get_the_content(), 18); ?>
+                            <?php endif; ?>
+                            <a href="<?php the_permalink();  ?>" class="nu gray">Learn more</a>
+                        </p>
                     </div>
                 </div>
             <?php endwhile; ?>
 
-            <p class="t-center no-margin"><a href="#" class="btn btn--blue">View All Events</a></p>
+            <p class="t-center no-margin"><a href="<?php echo get_post_type_archive_link("event"); ?>" class="btn btn--blue">View All Events</a></p>
         </div>
     </div>
     <div class="full-width-split__two">
@@ -54,7 +61,14 @@
                     </a>
                     <div class="event-summary__content">
                         <h5 class="event-summary__title headline headline--tiny"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h5>
-                        <p><?php echo wp_trim_words(get_the_content(15)); ?> <a href="<?php the_permalink(); ?>" class="nu gray">Read more</a></p>
+                        <p>
+                            <?php if (has_excerpt()) : ?>
+                                <?php echo get_the_excerpt(); ?>
+                            <?php else : ?>
+                                <?php echo wp_trim_words(get_the_content(), 18); ?>
+                            <?php endif; ?>
+                            <a href="<?php the_permalink(); ?>" class="nu gray">Read more</a>
+                        </p>
                     </div>
                 </div>
             <?php endwhile; ?>
