12221aa1bc8cb0435150ef5ea4a8824c68bf6537
beltranbot <beltranbot@gmail.com>
2022-09-21 20:17:56 -0500

    reducing duplicated code, creating the pageBanner function

--- functions.php
+++ functions.php
@@ -1,5 +1,30 @@
-<?php
+<?php function pageBanner($args = [])
+{ ?>
+  <?php // php logic will live here 
+    $title = !$args["title"] ? get_the_title() : $args["title"];
+    $subtitle = !$args["subtitle"] ? get_field("page_banner_subtitle") : $args["subtitle"];
+    $pageBannerImage = !$args["photo"] ? get_field("page_banner_background_image") : $args["photo"];
+    if (!$args["photo"]) {
+      if (get_field("page_banner_background_image")) {
+        $args["photo"] = get_field("page_banner_background_image")["sizes"]["pageBanner"];
+      } else {
+        $args["photo"] = get_theme_file_uri("/images/ocean.jpg");
+      }
+    }
+  ?>
+  <div class="page-banner">
+    <div class="page-banner__bg-image" style="background-image: url(<?php echo $args["photo"]; ?>);"></div>
+    <div class="page-banner__content container container--narrow">
+      <h1 class="page-banner__title"><?php echo $title; ?></h1>
+      <div class="page-banner__intro">
+        <p><?php echo $subtitle; ?></p>
+      </div>
+    </div>
+  </div>
 
+<?php } ?>
+
+<?php
 function university_files()
 {
   wp_enqueue_script("main-university-js", get_theme_file_uri("/build/index.js"), array("jquery"), "1.0", true);
